a9e598be2f045fa8f5b39d7fe300dba6f09c2b80
qa296 <3546639503@qq.com>
2025-11-04 21:02:48 +0800

    new file:   README.md

---
+++ unpackage/cache/wgt/H5581E90A/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  onLaunch() {
+    console.log('App Launch');
+  },
+  onShow() {
+    console.log('App Show');
+  },
+  onHide() {
+    console.log('App Hide');
+  }
+}
+</script>
+
+<style>
+/* 全局样式 */
+page {
+  background-color: #000000;
+  color: #ffffff;
+}
+</style>
